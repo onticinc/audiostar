376d107d3351e228425810d4cd8bdc8bc9ea7422
onticinc <onticinc@gmail.com>
2022-05-30 22:28:42 -0600

    debugged template links

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
-
-# Create your views here.
+    return render(request, 'blog/about.html', {'title': 'About'})
