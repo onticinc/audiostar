f3eb111549dd05fce2cdb755367a2869bc77e40b
onticinc <onticinc@gmail.com>
2022-05-30 19:18:38 -0600

    about view

--- app/blog/views.py
+++ app/blog/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
 
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
+
 # Create your views here.
